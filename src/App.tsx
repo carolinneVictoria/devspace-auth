8c4c60cfe53ef7cd99f373406e8d7b5e198d1d70
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-09-05 16:43:07 -0300

    feat: refactor App component to manage page state and navigation between Login, Signin, and Forgot Password

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import Login from "./components/auth/Login";
+import Signin from "./components/auth/Signin";
+import ForgotPassword from "./components/auth/forgot-password";
 
+type Page = "login" | "signin" | "forgot-password";
 
 function App() {
+  const [page, setPage] = useState<Page>("login");
+
+  if (page === "signin") {
+    return <Signin onLogin={() => setPage("login")} />;
+  }
+
+  if (page === "forgot-password") {
+    return <ForgotPassword onBack={() => setPage("login")} />;
+  }
+
   return (
-    <>
-      <Login />
-    </>
-  ) 
+    <Login
+      onCreateAccount={() => setPage("signin")}
+      onForgotPassword={() => setPage("forgot-password")}
+    />
+  )
 }
 
 export default App;
