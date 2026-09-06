fc418c7eeac50ce37d98f0003b56877ca394cf15
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-09-06 00:10:19 -0300

    feat: update App component structure and improve layout with Toaster

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Login from "./components/auth/Login";
-import Signin from "./components/auth/Signin";
-import ForgotPassword from "./components/auth/forgot-password";
-
-type Page = "login" | "signin" | "forgot-password";
+import Login from "./pages/auth/Login";
+import Signin from "./pages/auth/Signin";
+import ForgotPassword from "./pages/auth/forgot-password";
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace/>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="*" element={<Navigate to="/login" replace/>} />
-    </Routes>
+    <>
+      <Toaster richColors position="top-right" />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<Navigate to="/login" replace/>} />
+      </Routes>
+    </>
   )
 }
 
